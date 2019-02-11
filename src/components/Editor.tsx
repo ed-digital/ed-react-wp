031584e224ffbcb57cbc4189550e0aab0e569c87
brynshanahan <bryn@ed.com.au>
2019-02-11 12:22:22 +1100

    Add InnerBlocks

---
+++ src/components/Editor.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement } from 'react'
+
+interface RichTextProps {
+  onChange : (text: string) => void,
+  tagName: string,
+  value: string
+  placeholder?: string,
+  multiline?: keyof HTMLElementTagNameMap,
+}
+
+export function RichText (props:RichTextProps) { 
+  const WPRichText = window.wp.editor.RichText
+  return <WPRichText {...props}/>
+}
+
+interface InnerBlocks {
+  allowedBlocks?: string[]
+}
+
+export function InnerBlocks (props:InnerBlocks) {
+  const WPInnerBlocks = window.wp.editor.InnerBlocks
+  return <WPInnerBlocks {...props}/>
+}
